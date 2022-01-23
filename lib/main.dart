d257464da29a50a4b5ebccf0b80c1debc8793898
JuanCPDev <juan_perez@knights.ucf.edu>
2022-01-23 03:24:19 -0500

    initial setup ios

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'inventoryscreen.dart';
-import 'dart:async';
+
 
 late final db;
 late final database;
